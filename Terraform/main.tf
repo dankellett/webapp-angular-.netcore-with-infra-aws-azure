0b493dbf23aa6e28b38d3d6e9fe974e3a86c61c8
Dan Kellett <dan.kellett@outlook.com>
2020-05-12 16:50:15 -0700

    Add Infrastructure Code Deployment (Terraform)

---
+++ Terraform/main.tf
@@ -0,0 +1,104 @@
+﻿# Define Terraform provider
+terraform {
+  required_version = ">= 0.12"
+}
+# Configure the Azure provider
+provider "azurerm" { 
+  environment = "public"
+  version = "=2.9.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "West US"
+}
+
+resource "azurerm_storage_account" "rg" {
+  name                     = "tempkellett567"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_sql_server" "rg" {
+  name                         = "kellett-template-sqlserver"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.database_admin_username
+  administrator_login_password = var.database_admin_password
+
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.rg.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.rg.primary_access_key
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 6
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_sql_database" "rg" {
+  name                = "mysqldatabase"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = "West US"
+  server_name         = azurerm_sql_server.rg.name
+
+  extended_auditing_policy {
+    storage_endpoint                        = azurerm_storage_account.rg.primary_blob_endpoint
+    storage_account_access_key              = azurerm_storage_account.rg.primary_access_key
+    storage_account_access_key_is_secondary = true
+    retention_in_days                       = 6
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_app_service_plan" "rg" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "rg" {
+  name                = var.app_service_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.rg.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "None"
+  }
+
+  app_settings = {
+    "app_global_admin_username" = var.app_global_admin_username
+    "app_global_admin_password" = var.app_global_admin_password
+    "auth_cert_thumbprint" = var.auth_cert_thumbprint
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLAzure"
+    value = "Server=tcp:${azurerm_sql_server.rg.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_database.rg.name};Persist Security Info=False;User ID=${azurerm_sql_server.rg.administrator_login};Password=${azurerm_sql_server.rg.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
+}
+
+resource "azurerm_app_service_certificate" "rg" {
+  name                = "auth-cert"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  pfx_blob            = filebase64(var.auth_cert_filepath)
+  password            = var.auth_cert_password
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,56 @@
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resorce group for all resources in this project."
+}
+
+variable "database_admin_username" {
+  type        = string
+  description = "The username to be set for the database administrator."
+}
+
+variable "database_admin_password" {
+  type        = string
+  description = "The password to be set for the database administrator."
+}
+
+variable "app_service_name" {
+  type        = string
+  description = "The name to be used for the Azure App Service. This must be unique to all Azure and is exposed on the app URL."
+}
+
+variable "app_service_plan_name" {
+  type        = string
+  description = "The name to be used for the Azure App Service Plan."
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "The name to be used for the Azure App Service. This must be unique to all Azure."
+}
+
+variable "auth_cert_password" {
+  type        = string
+  description = "The password for the auth certificate to be installed."
+}
+
+variable "auth_cert_filepath" {
+  type        = string
+  description = "The full path for the auth certificate to be installed."
+  default     = "../auth_key_dev.pfx"
+}
+
+variable "auth_cert_thumbprint" {
+  type        = string
+  description = "The thumbprint of auth certificate to be installed - the applicaiton uses this to find the cert in the cert store."
+}
+
+variable "app_global_admin_username" {
+  type        = string
+  description = "The username for the application global admin user. It must be an email."
+}
+
+variable "app_global_admin_password" {
+  type        = string
+  description = "The password for the application global admin user. It must meet typical strength properties."
+}
+
